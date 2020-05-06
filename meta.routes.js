5a2c71fc91957b938d337c57af5cc921e6e1a448
Dandy <dandyjreyes@gmail.com>
2020-05-05 20:56:08 -0700

    Checking if heroku is involved in getting information from master branch

--- meta.routes.js
+++ meta.routes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const usersRouter = require('./routes');
 
+// Route for the user information
 app.use('/users', usersRouter);
 
 module.exports = app;
